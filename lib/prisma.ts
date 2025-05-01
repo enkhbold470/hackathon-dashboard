155b938455f9b4d853f388fae1fcc0f71bcbc1a6
Enkhbold Ganbold <enkhbold470@gmail.com>
2025-04-30 17:45:52 -0700

    Update Prisma import: Change import statement from generated Prisma client to '@prisma/client' for improved compatibility and maintainability.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // lib/prisma.ts
-import { PrismaClient } from '../lib/generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from "@prisma/extension-accelerate";
 
 const globalForPrisma = globalThis as typeof globalThis & {
